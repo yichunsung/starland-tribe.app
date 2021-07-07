f5e9b5e9f90aedf1a1f9261ff8e1841ee0c52d12
YiChun Sung <ycsung.r@gmail.com>
2021-07-08 03:10:49 +0800

    Added game canvas and pixi package.

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
+import * as PIXI from 'pixi.js';
 
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
   styleUrls: ['./game.component.scss']
 })
-export class GameComponent implements OnInit {
+export class GameComponent implements AfterViewInit {
+
+  @ViewChild('gameCvs', {static: false}) gameCvs?: ElementRef;
+
+  public context?: CanvasRenderingContext2D;
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+    this.context = this.gameCvs?.nativeElement.getContext('2d');
+  }
+
+  check() {
+    console.log(this.gameCvs);
+    console.log(this.context);
   }
 
 }
